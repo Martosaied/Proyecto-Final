503bd1642314352dedb19e62271115bb584596ae
Martosaied <martinsaied@gmail.com>
2017-06-28 23:53:06 -0300

    CAMBIO TOTAL A ENTITY FRAMEWORK

---
+++ ProyectoFinal/PFEF/Controllers/UsuariosController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PFEF.Models;
+using PFEF.ViewModels;
+using AutoMapper;
+
+namespace PFEF.Controllers
+{
+    public class UsuariosController : Controller
+    {
+
+        private dbEntities1 db = new dbEntities1();
+
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            return View(db.Usuarios.ToList());
+        }
+
+
+        // GET: Usuarios/Create
+        public ActionResult Registracion()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Registracion([Bind(Include = "Nombre,Apellido,Email,Contraseña,confirmarContraseña")] RegistracionViewModel NewUser)
+        {
+            if (ModelState.IsValid)
+            {
+                var config = new MapperConfiguration(cfg => {
+                    cfg.CreateMap<RegistracionViewModel, Usuarios>();
+                });
+                IMapper mapper = config.CreateMapper();
+                var User = mapper.Map<RegistracionViewModel, Usuarios>(NewUser);
+                db.Usuarios.Add(User);
+                db.SaveChanges();
+                return RedirectToAction("Index","Home");
+            }
+
+            return View(NewUser);
+        }
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel objUser)
+        {
+            if (ModelState.IsValid)
+            {
+                using (dbEntities1 db = new dbEntities1())
+                {
+                    var obj = db.Usuarios.Where(a => a.Email.Equals(objUser.Email) && a.Contraseña.Equals(objUser.Contraseña)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["User"] = obj;
+                        return RedirectToAction("HomeUsuario");
+                    }
+                }
+            }
+            return View(objUser);
+        }
+
+        [HttpGet]
+        public ActionResult HomeUsuario()
+        {
+            return View("HomeUsuario");
+        }
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProyectoFinal/PFEF/ViewModels/ContenidosViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using PFEF.Models;
+
+namespace PFEF.ViewModels
+{
+    public class DropsCharger
+    {
+        public List<Escuelas> dropEscuela { get; set; }
+        public List<Materias> dropMateria { get; set; }
+        public List<TiposContenidos> dropTipoContenido { get; set; }
+        public List<NivelesEducativos> dropNivelEducativo { get; set; }
+    }
+    public class BuscadorViewModel
+    {
+        public string KeyWords { get; set; }
+    }
+    public class SubirViewModel : DropsCharger
+    {
+
+
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Descripcion { get; set; }
+        public string Ruta { get; set; }
+        [Required]
+        public string Profesor { get; set; }
+        [Required]
+        public string Cursada { get; set; }
+
+        public int IdUsuario { get; set; }
+        [Required]
+        public int IdEscuela { get; set; }
+        [Required]
+        public int IdMateria { get; set; }
+        [Required]
+        public int IdTipoContenido { get; set; }
+        [Required]
+        public int IdNivelEducativo { get; set; }
+    }
+    public class FiltrarViewModel : DropsCharger
+    {
+        public string Nombre { get; set; }
+        public string Profesor { get; set; }
+        public string Cursada { get; set; }
+        public int IdUsuario { get; set; }
+        public int IdEscuela { get; set; }
+        public int IdMateria { get; set; }
+        public int IdTipoContenido { get; set; }
+        public int IdNivelEducativo { get; set; }
+
+
+        //Con esto llenamos los drop en un objeto y mostrar las listas desde un objeto creado sin la necesidad de utilizar viewbags
+
+    }
+}
